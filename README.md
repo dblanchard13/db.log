b00dcdc92c2ccc5bf7ac32d670bd4b631dbcf451
dblanchard13 <dblanchard@gobalto.com>
2016-05-06 17:11:46 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# db.log
+
+****
+
+Simple Logger for Logging info or errors with contrasting colors and some emojis.
+
+****
+
+## Installation
+
+```
+  npm install db.log
+```
+
+****
+
+## Example Usage
+
+```
+  var log = require('db.log');
+
+  log.info('WILDLY USEFUL INFORMATION!!');
+
+  log.error('OH NOES!!');
+```
+
+****
+
+## Config
+
+Logging can be turned off by setting `process.env.loggingOff = true`
---
+++ index.js
@@ -0,0 +1,45 @@
+module.exports = (function(){
+
+  'use strict';
+
+  require('colors');
+  var _ = require('lodash');
+  var noLog = process.env.loggingOff;
+
+  function noOp(){};
+  var consoleLog = noLog ? noOp : console.log.bind(console);
+
+  var logger = {
+
+    info: function() {
+      var tag = '[ ✨ LOG ✨ ]'.green;
+      var args = _.toArray(arguments)
+        .map(function(arg) {
+          if(typeof arg === 'object') {
+            var string = JSON.stringify(arg, null, 2);
+            return tag + '  ' + string.cyan;
+          }
+          else {
+            return tag + '  ' + arg.cyan;
+          }
+        });
+
+      consoleLog.apply(console, args);
+    },
+
+    error: function() {
+      var args = _.toArray(arguments)
+        .map(function(arg) {
+          arg = arg.stack || arg;
+          var name = arg.name || '[ ❌ ERROR ❌ ]';
+          var log = name.yellow + '  ' + arg.red;
+          return log;
+        });
+
+      consoleLog.apply(console, args);
+    }
+  };
+
+  return logger;
+
+}());
